c98417aaf42d761faca6990b46adc7cda6cf8bad
Adarsh Raj <adarshraj.dev@gmail.com>
2024-03-23 23:48:36 +0530

    Build changes

--- src/main/java/in/adarshr/targetcloner/helper/CompareHelper.java
+++ src/main/java/in/adarshr/targetcloner/helper/CompareHelper.java
@@ -7,6 +7,7 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +19,7 @@ import java.util.Map;
 public class CompareHelper {
 
     private static final Logger LOG = LoggerFactory.getLogger(CompareHelper.class);
+    private static final String FIELD_COMPARISON_FORMAT = "Field Name: %s - Object1: %s | Object2: %s";
 
     /**
      * Private constructor to prevent instantiation
@@ -42,4 +44,35 @@ public class CompareHelper {
         }
     }
 
+    /**
+     * Compares the fields of two objects.
+     *
+     * @param obj1 the first object
+     * @param obj2 the second object
+     */
+    @SuppressWarnings("unused")
+    public void compareObjectFields(Object obj1, Object obj2) {
+        Field[] fields = obj1.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            try {
+                field.setAccessible(true); //NOSONAR
+                Object value1 = field.get(obj1);
+                Object value2 = field.get(obj2);
+                compareFieldValues(field, value1, value2);
+            } catch (IllegalAccessException e) {
+                LOG.error("Failed to compare object fields: {}", e.getMessage());
+            }
+        }
+    }
+
+    private void compareFieldValues(Field field, Object value1, Object value2) {
+        if (value1 != null && value2 != null && !value1.equals(value2)) {
+            logFieldComparison(field, value1, value2);
+        }
+    }
+
+    private void logFieldComparison(Field field, Object value1, Object value2) {
+        LOG.info(String.format(FIELD_COMPARISON_FORMAT, field.getName(), value1, value2)); //NOSONAR
+    }
 }
+
